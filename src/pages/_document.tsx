8d1a5bb515c5ae986d8cc8e94daf6c662bda2695
xdpatrykxd <xdpatrykxdofc@gmail.com>
2025-05-10 20:41:38 +0200

    Test site

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,5 @@
 import { Metadata } from "next";
 import { Html, Head, Main, NextScript } from "next/document";
-export const metadata: Metadata = {
-  title: "Antwerpse Hondenweiden",
-  description: "Find the best off-leash dog areas in and around Antwerp",
-}
 export default function Document() {
   return (
     <Html lang="en">
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -15,21 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>
-          <nav className="navbar">
-            <div className="logo">
-              <a href="/">Antwerpse Hondenweiden</a>
-            </div>
-            <ul className="nav-links">
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/sponsors">Sponsors</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        
         {children}
         <footer>
           <p>&copy; {new Date().getFullYear()} Antwerpse Hondenweiden. All rights reserved.</p>
